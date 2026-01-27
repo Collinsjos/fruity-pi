73817b393599a6d7f5bab743abc2b75aee3e6f54
Collinsjos <collinsjose82@gmail.com>
2026-01-27 14:19:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Connects the React App to the <div id="root"> in index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
